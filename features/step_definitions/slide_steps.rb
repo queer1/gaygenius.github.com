028671946e9576a75dc2fdcaee813479e745729a
José J. Capó <jose@josecapo.com>
2012-03-10 13:23:37 -0800

    DRYer slide steps

--- features/step_definitions/slide_steps.rb
+++ features/step_definitions/slide_steps.rb
@@ -1,3 +1,8 @@
+def is_current_slide?(slide_number)
+  slide_index = slide_number.to_i - 1
+  page.evaluate_script("$.deck('getSlide', #{slide_index}).hasClass('deck-current')")
+end
+
 When /^I click on the right arrow$/ do
   page.execute_script("$('.deck-next-link').show()")
   find('.deck-next-link').click
@@ -5,13 +10,11 @@ end
 
 Then /^I should be on the (\d+)(?:st|nd|th) cv slide$/ do |slide_number|
   current_path.should == '/cv/slides.html'
-  slide_index = slide_number.to_i - 1
-  page.evaluate_script("$.deck('getSlide', #{slide_index}).hasClass('deck-current')").should be_true
+  is_current_slide?(slide_number).should be_true
 end
 
 Then /^I should not be on the (\d+)(?:st|nd|th) cv slide$/ do |slide_number|
   if current_path == '/cv/slides.html'
-    slide_index = slide_number.to_i - 1
-    page.evaluate_script("$.deck('getSlide', #{slide_index}).hasClass('deck-current')").should be_false
+    is_current_slide?(slide_number).should be_false
   end
 end
